bd350bf08fb5732f5e33fdebaf17ebdd50730dac
l4u <louyuhong@gmail.com>
2012-02-27 15:40:00 +0800

    Initial extract

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh script/jruby --1.9 -S trinidad -p $PORT -e production --config 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJrubyHeroku::Application.config.secret_token = '3b27eb36873f3b9937ec2a852edd81daaf90186a9d331eb67fb50450bfee78fbd88b1f2d04e4731872c6262f03b318dff2547213084babcf786cf6407cd2d5c7'
